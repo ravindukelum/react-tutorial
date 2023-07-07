c180719337bfff92449c28347100788637a56373
W.A.R.K Weerasooriya <67100910+ravindukelum@users.noreply.github.com>
2023-07-07 09:02:16 +0530

    useeffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
-const quotes = [
-  "if you are good at something, never do it for free",
-  "the night is darkest just before the dawn",
-  "you either die a hero or live long enough to see yourself become the villain",
-  "it is not who i am underneath, but what i do that defines me",
-  "you have nothing, nothing to threaten me with, nothing to do with all your strength",
-  "i am not wearing hockey pads",
-  "i am batman",
-  "i am not affraid of man who practice 1000 kicks 1 time ,but rather 1 kick 1000 times",
-];
 
 function App() {
-  const [quote, setQuotes] = useState(quotes[0]);
 
-  function randomizeQuote() {
-    const randomQuote = Math.floor(Math.random() * quotes.length);
-    setQuotes(quotes[randomQuote]);
-  }
+    const [count, setCount] = useState(0);
+            console.log("I was Ran",count)
+    useEffect((i)=>{
+
+    },[])
+
+
+
+    function increment() {
+        setCount(count + 1);
+    }
+
+    function decrement() {  
+        setCount(count - 1);
+    }
 
   return (
     <div>
-      <div>{quote}</div>
-      <button onClick={randomizeQuote}>Click Me</button>
+      <h1>Counter : {count}</h1>
+      <button onClick={increment}>add conter</button>
+      <button onClick={decrement}>Sub counter</button>
     </div>
   );
 }
