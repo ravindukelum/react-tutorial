345e4cdd2f30ffb0e1d3afb664f84bf0b6ebc8b4
W.A.R.K Weerasooriya <67100910+ravindukelum@users.noreply.github.com>
2023-07-07 08:08:13 +0530

    random

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,28 @@
 import React, { useState } from "react";
-import Custom from "./Custom";
 
-console.log("HEllo world");
-
-// setCounter = function (value) {   //this is the function that is called when we call setCounter
-//   if (typeof value === "function") {
-//     // I want to something else
-//     const updatedValue = value(oldValue);
-//     counter = updatedValue;
-//     rerender();
-//   } else {
-//     counter = value;
-//     rerender();
-//   }
-// }
+const quotes = [
+  "if you are good at something, never do it for free",
+  "the night is darkest just before the dawn",
+  "you either die a hero or live long enough to see yourself become the villain",
+  "it is not who i am underneath, but what i do that defines me",
+  "you have nothing, nothing to threaten me with, nothing to do with all your strength",
+  "i am not wearing hockey pads",
+  "i am batman",
+  "i am not affraid of man who practice 1000 kicks 1 time ,but rather 1 kick 1000 times",
+];
 
 function App() {
-  const [counter, setCounter] = useState(0); //start with use call hooks
+  const [quote, setQuotes] = useState(quotes[0]);
 
-  function addcounter() {
-    setCounter(oldCounterValue=>oldCounterValue+1);
-  }
-
-  function subcounter() {
-    setCounter(counter - 1);
+  function randomizeQuote() {
+    const randomQuote = Math.floor(Math.random() * quotes.length);
+    setQuotes(quotes[randomQuote]);
   }
 
   return (
     <div>
-      <h1>Counter {counter}</h1>
-      <button onClick={addcounter}>Increase counter</button>
-      <button onClick={subcounter}>Decrease counter</button>
-
-      <Custom name="Ravindu" />
+      <div>{quote}</div>
+      <button onClick={randomizeQuote}>Click Me</button>
     </div>
   );
 }
--- src/components/Custom.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Custom(props) {
-  return <p>Hello , {props.name} </p>;
-}
-
-export default Custom;
