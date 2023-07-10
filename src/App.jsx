40e1a6226fa48d38338c435e84f631432c7e94f4
W.A.R.K Weerasooriya <67100910+ravindukelum@users.noreply.github.com>
2023-07-10 12:10:42 +0530

    route

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import User from './User.jsx'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/hello-world">go to Hello World</Link>
+            </li>
+            <li>
+              <Link to="/">Go home</Link>
+            </li>
+            <li>
+              <Link to="/user/ravi">use1</Link>
+            </li>
+            <li>
+              <Link to="/user/John">use2</Link>
+            </li>
+            <li>
+              <Link to="/user/ant">use3</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/hello-world" element={<h1>Hello World route</h1>} />
+          <Route path="/" element={<h1>I am on Homepage</h1>} />
+          <Route path="/user/:username" element={<User />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-//alert(add(1, 2));
-//alert(sub(1, 2));
-//alert(multiply(1, 2));
-
 root.render(
   <App />
-
 );
 
  
